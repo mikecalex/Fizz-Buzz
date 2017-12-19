3f264bad76ffc831ac25b9eba6e304c73a486178
mikecalex <mikecalex@gmail.com>
2017-12-18 20:27:03 -0500

    Init Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Basic Ruby program that prints out the number 1-100 but replaces any number divisible by three with 'Fizz', by five with 'Buzz', and three and five with 'FizzBuzz'.
---
+++ fizz_buzz.rb
@@ -0,0 +1,14 @@
+number = 1
+
+while number < 101 do
+  if number % 3 == 0 && number % 5 == 0
+    puts 'FizzBuzz'
+  elsif number % 5 == 0
+    puts 'Buzz'
+  elsif number % 3 == 0
+    puts 'Fizz'
+  else
+    puts number
+  end
+  number += 1
+end
